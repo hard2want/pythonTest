f157c42d74120253a55475817b8aa55d701f44f7
Duke <dbest1@ccc.edu>
2018-10-31 12:36:34 -0500

    added comments and moved redundant if/else statement

--- NetPay.py
+++ NetPay.py
@@ -1,15 +1,17 @@
-name=input("What is your name? ")
-hourlyPay=float(input("How much do you get paid by the hour? "))
+# promt user for their name
+name = input("What is your name? ")
+
+# prompt user for their hourly rate, remember to make it a float
+hourlyPay = float(input("How much do you get paid by the hour? "))
+
+
 overtimeHourlyPay=hourlyPay*1.5
 weeklyHoursWorked=float(input("How many hours do you work each week? "))
 if weeklyHoursWorked <= 40:
     weeklyOvertimeHoursWorked=0
 else:
     weeklyOvertimeHoursWorked=weeklyHoursWorked-40
-#if weeklyHoursWorked <=40:
-#    weeklyHoursWorked=weeklyHoursWorked
-#else:
-#    weeklyHoursWorked=40
+
 if weeklyHoursWorked+weeklyOvertimeHoursWorked <= 40:
    weeklySalary=weeklyHoursWorked*hourlyPay
 else: 
@@ -19,3 +21,8 @@ taxes=grossSalary*.1
 netPay=grossSalary-taxes
 print ( name, "You earn", hourlyPay, " dollars an hour and work", weeklyHoursWorked+weeklyOvertimeHoursWorked, "hours a week. Your gross pay comes out to", grossSalary, "After paying", taxes, "dollars in taxes, your net pay comes out to", netPay)
 
+
+#if weeklyHoursWorked <=40:
+#    weeklyHoursWorked=weeklyHoursWorked
+#else:
+#    weeklyHoursWorked=40
